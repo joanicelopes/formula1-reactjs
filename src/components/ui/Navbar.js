c76b33ce93807f36d749d61a7bb9290314a30b11
estagiario.dti1 <estagiario.dti1@SID-NOTB-031>
2023-06-09 16:00:41 -0100

    update last race results

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -3,7 +3,7 @@ import logo from '../../img/F1-icon-square.png'
 export default function Navbar() {
     return (
         <nav className="nav">
-            <a href="/" className="site-title"><img src={logo} className="img-responsive" alt="" /></a>
+            <a href="/" className="site-title"><img src={logo} className="img-responsive" alt="Home" /></a>
             <ul>
                 <li>
                     <a href="/drivers">Drivers</a>
